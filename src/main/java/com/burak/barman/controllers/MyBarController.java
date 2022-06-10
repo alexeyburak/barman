be0ff915050dd968662c9deb14d23785b3e953e5
Alexey Burak <byraklesha@gmail.com>
2022-06-10 17:00:35 +0300

    feat: add navigation between menu's

--- src/main/java/com/burak/barman/controllers/MyBarController.java
+++ src/main/java/com/burak/barman/controllers/MyBarController.java
@@ -1,9 +1,13 @@
 package com.burak.barman.controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import static com.burak.barman.ChangeScene.changeScene;
 
 /**
  * Barman
@@ -11,8 +15,24 @@ import java.util.ResourceBundle;
  */
 
 public class MyBarController implements Initializable {
+
+    @FXML Button buttonBack;
+    @FXML Button buttonConstructor;
+    @FXML Button buttonIngredients;
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        // Go to main menu
+        buttonBack.setOnAction(event -> changeScene(event, "mainStage.fxml"));
+
+        // Go to constructor menu
+        buttonConstructor.setOnAction(event -> changeScene(event, "constructor.fxml"));
+
+        // Go to ingredients menu
+        buttonIngredients.setOnAction(event -> changeScene(event, "ingredient.fxml"));
+
 
     }
 }
